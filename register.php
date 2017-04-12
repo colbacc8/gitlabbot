1103fddcad30e497c188511ddff1205253cf0dae
colbacc8 <marco.proca@gmail.com>
2017-04-12 10:00:00 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://gitlabbot.herokuapp.com/execute.php';
+$BOT_TOKEN = '303627578:AAEW-dfMLujBh4j97DHz_0hoAHOKiIV5vIo';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
